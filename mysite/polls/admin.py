3c45bad4101ccd066f4baf2c2824ace395be0ec9
Joaquín Fernando Ortega Silva <jf.ortega.silva@gmail.com>
2023-07-11 19:18:46 -0600

    changes to improve thw website's layout and functionality

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
-# Register your models here.
-admin.site.register(Question)
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Question text",{"fields": ["question_text"]}), 
+        ("Date information",{"fields": ["pub_date"], "classes":["collapse"]}),
+    ]
+
+    inlines=[ChoiceInLine]
+
+    list_display = ["question_text","pub_date","was_published_recently "]
+
+    # fieldsets -> son bandas de subtitlos que se muestran en el navegador 
+    # This tells Django: “Choice objects are edited on the Question admin page. By default, provide enough fields for 3 choices.”
+
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice) # <- removed
+
