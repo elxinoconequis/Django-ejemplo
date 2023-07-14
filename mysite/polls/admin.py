74dc60252562820f6a6bc2584cbb5aedbb4b33ef
Joaquín Fernando Ortega Silva <jf.ortega.silva@gmail.com>
2023-07-14 11:40:29 -0600

    modify admin menu

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -11,15 +11,22 @@ class QuestionAdmin(admin.ModelAdmin):
         ("Question text",{"fields": ["question_text"]}), 
         ("Date information",{"fields": ["pub_date"], "classes":["collapse"]}),
     ]
-
-    inlines=[ChoiceInLine]
-
-    list_display = ["question_text","pub_date","was_published_recently "]
-
     # fieldsets -> son bandas de subtitlos que se muestran en el navegador 
     # This tells Django: “Choice objects are edited on the Question admin page. By default, provide enough fields for 3 choices.”
 
+    inlines=[ChoiceInLine]
+
+    list_display = ["question_text","pub_date","was_published_recently"]
+    list_filter = ["pub_date"] # That adds a “Filter” sidebar that lets people filter the change list by the pub_date field:
+    search_fields = ["question_text"]
+    # list_per_page = 1 # funciona
+    # date_hierarchy ="pub_date" # no funciona
+
+
+    
 
 admin.site.register(Question, QuestionAdmin)
 # admin.site.register(Choice) # <- removed
 
+# Find: Django source files, with the following command
+#  python -c "import django; print(django.__path__)"
